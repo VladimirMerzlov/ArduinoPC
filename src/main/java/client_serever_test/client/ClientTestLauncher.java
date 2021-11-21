3088082f4ba1cd41e094eb6972298372999facd5
Merzlov V.V <vladimir_merzlov@mail.ru>
2021-11-21 22:49:39 +0300

    setting up ClientTest -> ServerTest

--- src/main/java/client_serever_test/client/ClientTestLauncher.java
+++ src/main/java/client_serever_test/client/ClientTestLauncher.java
@@ -6,20 +6,17 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class ClientTestLauncher {
-
-    private static String hostSet = "localhost"; // Хост для подключения клиента к серверу
-    private static int port = 5000;              // Порт для подключеия клиента к серверу
-
     public static void main(String[] args) {
         try {
-            InetAddress host = InetAddress.getByName(hostSet);
+            InetAddress host = InetAddress.getByName("localhost");
+            int port = 5000;
 
             System.out.println(" <<< Client is ready >>> ");
             System.out.println("Enter the command : ");
 
             //while(true){
-            ClientTestB client = new ClientTestB(host, port);
-            client.start();
+                ClientTestB client = new ClientTestB(host, port);
+                client.start();
             //}
 
 
--- src/main/java/client_serever_test/client/Controller.java
+++ src/main/java/client_serever_test/client/Controller.java
@@ -1,4 +1,30 @@
 package client_serever_test.client;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ToggleButton onoffbtn;
+
+    @FXML
+    void initialize() {
+        boolean a = false;
+        onoffbtn.setSelected(a);
+        onoffbtn.setOnAction(event -> {
+            onoffbtn.setSelected(!a);
+            System.out.println("Кнопка нажата ? " + a);
+        });
+
+    }
 }
